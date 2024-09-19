ea158e5a413712e50be7bd86a4155818e00f8fea
Darnell Green <123552242+G1320@users.noreply.github.com>
2024-09-19 13:18:23 +0300

    Sign in / sign out button now displays correct state

--- src/components/auth/logout-button.tsx
+++ src/components/auth/logout-button.tsx
@@ -2,7 +2,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from '@/components'
 
 export const LogoutButton = () => {
-  const { logout, isAuthenticated } = useAuth0();
+  const { user, logout, isAuthenticated } = useAuth0();
 
   const handleLogout = () => {
     logout();
@@ -10,7 +10,7 @@ export const LogoutButton = () => {
   };
 
   return (
-    isAuthenticated ? (
+    user && isAuthenticated ? (
       <Button onClick={handleLogout} className="button login button">
         Sign out
       </Button>
