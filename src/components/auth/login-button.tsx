34df98d7f21a9303bc9a44183244dc0478fe43b4
Darnell Green <123552242+G1320@users.noreply.github.com>
2024-10-02 11:14:42 +0300

    Login and logout button now display correct version based on isAuthenticated via auth0

--- src/components/auth/login-button.tsx
+++ src/components/auth/login-button.tsx
@@ -62,7 +62,7 @@ export const LoginButton: React.FC = (): ReactElement | null => {
   }, [isAuthenticated, user]);
 
   return (
-    !isAuthenticated && !user ? (
+    !isAuthenticated ? (
       <Button onClick={handleClick} className="button login button">
         Sign in
       </Button>
--- src/components/auth/logout-button.tsx
+++ src/components/auth/logout-button.tsx
@@ -2,7 +2,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from '@/components'
 
 export const LogoutButton = () => {
-  const { user, logout, isAuthenticated } = useAuth0();
+  const { logout, isAuthenticated } = useAuth0();
 
   const handleLogout = () => {
     logout();
@@ -10,7 +10,7 @@ export const LogoutButton = () => {
   };
 
   return (
-    user && isAuthenticated ? (
+    isAuthenticated ? (
       <Button onClick={handleLogout} className="button login button">
         Sign out
       </Button>
