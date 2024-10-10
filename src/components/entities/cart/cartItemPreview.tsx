f0748625e43b6507a3c99b6500d83818be2009b0
Darnell Green <123552242+G1320@users.noreply.github.com>
2024-10-10 13:34:10 +0300

    Removed unused CDN import for Slick

--- src/components/entities/cart/cartItemPreview.tsx
+++ src/components/entities/cart/cartItemPreview.tsx
@@ -1,43 +1,3 @@
-// import { useNavigate } from 'react-router-dom';
-// import { Button } from '@/components';
-// import { CartItem } from '@/types/index';
-// import { formatBookingDate } from '@/utils';
-
-// interface CartItemPreviewProps {
-//   item: CartItem;
-//   onRemoveFromCart: (item: CartItem) => void;
-// }
-
-// export const CartItemPreview: React.FC<CartItemPreviewProps> = ({ item, onRemoveFromCart }) => {
-  
-//   const navigate = useNavigate();
-
-//   const handleClick = (e: React.MouseEvent<HTMLElement>) => {
-//     const target = e.target as HTMLElement;
-//     if (target.nodeName !== 'BUTTON') {
-//       navigate(`/item/${item?.itemId}`);
-//     }
-//   };
-
-//   return (
-//     <article onClick={handleClick} className="preview cart-item-preview">
-//       <h3 onClick={handleClick}>{item?.name}</h3>
-//       <p>{item?.studioName}</p>
-//       <small>{formatBookingDate(item?.bookingDate)}</small>
-//       <div>
-//         <small className='cart-item-preview-price' onClick={handleClick}>${item?.price?.toFixed(2)}</small>
-//         <small className='cart-item-preview-quantity' onClick={handleClick}>Hours: {item.quantity}</small>
-//       </div>
-//       <Button onClick={() => onRemoveFromCart(item)} className="remove-from-cart">
-//         Remove
-//       </Button>
-//     </article>
-//   );
-// };
-
-// export default CartItemPreview;
-
-
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@/components';
 import { CartItem } from '@/types/index';
@@ -77,7 +37,7 @@ export const CartItemPreview: React.FC<CartItemPreviewProps> = ({ item, onDecrem
     }
   };
 
-  const handleRemoveFromCart = (e: React.MouseEvent, item: CartItem) => {
+  const handleDecrementQuantity = (e: React.MouseEvent, item: CartItem) => {
     e.stopPropagation();
     onDecrementQuantity(item);
   };
@@ -91,7 +51,7 @@ export const CartItemPreview: React.FC<CartItemPreviewProps> = ({ item, onDecrem
         <small className='cart-item-preview-price' onClick={handleClick}>Price: ${item?.price?.toFixed(2)}</small>
       </div>
       <div>
-        <Button onClick={(e) => handleRemoveFromCart(e, item)} className="remove-from-cart"> - </Button>
+        <Button onClick={(e) => handleDecrementQuantity(e, item)} className="remove-from-cart">-</Button>
         <small className='cart-item-preview-quantity' onClick={handleClick}>Hrs: {item.quantity}</small>
         <Button onClick={handleIncrementQuantity} className="increment-quantity">+</Button>
       </div>
