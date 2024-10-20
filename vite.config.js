fa6fd76916abe3eb1d4b1621ba3e770a199e958a
Darnell Green <123552242+G1320@users.noreply.github.com>
2024-10-20 12:51:57 +0300

    Add robots.txt and sitemap via vite-plugin-sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Sitemap({ hostname: 'https://studioz.co.il' })],
   build: {
     outDir: 'dist'
   },
