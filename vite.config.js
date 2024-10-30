c78fa139a5d2d9b32ab326e33e90733aacb6053f
Darnell Green <123552242+G1320@users.noreply.github.com>
2024-10-30 08:07:55 +0200

    Update tsconfig, add tsconfigPaths

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import Sitemap from 'vite-plugin-sitemap';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Sitemap({ hostname: 'https://studioz.co.il' })],
+  plugins: [react(), tsconfigPaths(), Sitemap({ hostname: 'https://studioz.co.il' })],
   build: {
     outDir: 'dist'
   },
