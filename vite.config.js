fa340cd8b2451a1b6b784c86313a6fe6c96682a6
Darnell Green <123552242+G1320@users.noreply.github.com>
2024-10-30 08:22:04 +0200

    Update vite comfig

--- vite.config.js
+++ vite.config.js
@@ -9,18 +9,18 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths(), Sitemap({ hostname: 'https://studioz.co.il' })],
   build: {
     outDir: 'dist'
-  },
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, './src/components'),
-      '@services': path.resolve(__dirname, './src/services'),
-      '@contexts': path.resolve(__dirname, './src/contexts'),
-      '@forms': path.resolve(__dirname, './src/forms'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@models': path.resolve(__dirname, './src/models'),
-      '@config': path.resolve(__dirname, './src/config')
-    }
   }
+  // resolve: {
+  //   alias: {
+  //     '@components': path.resolve(__dirname, './src/components'),
+  //     '@services': path.resolve(__dirname, './src/services'),
+  //     '@contexts': path.resolve(__dirname, './src/contexts'),
+  //     '@forms': path.resolve(__dirname, './src/forms'),
+  //     '@hooks': path.resolve(__dirname, './src/hooks'),
+  //     '@pages': path.resolve(__dirname, './src/pages'),
+  //     '@utils': path.resolve(__dirname, './src/utils'),
+  //     '@models': path.resolve(__dirname, './src/models'),
+  //     '@config': path.resolve(__dirname, './src/config')
+  //   }
+  // }
 });
