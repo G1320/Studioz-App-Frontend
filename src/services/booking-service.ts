831a3276a97d251ff09b20a655d61f7b646bd694
Darnell Green <123552242+G1320@users.noreply.github.com>
2024-10-30 09:30:24 +0200

    Add logs to bookItem service

--- src/services/booking-service.ts
+++ src/services/booking-service.ts
@@ -4,8 +4,8 @@ import { CartItem } from '@models/index';
 const bookingEndpoint = '/bookings';
 
 export const bookItem = async (item: CartItem, userId: string) => {
-  // console.log('userId: ', userId);
-  // console.log('item: ', item);
+  console.log('userId: ', userId);
+  console.log('item: ', item);
   try {
     return await httpService.post(`${bookingEndpoint}/${userId}/book-item`, item);
   } catch (error) {
