ae6bc481cf0194cd3a7aff9a8449faea90f18199
Darnell Green <123552242+G1320@users.noreply.github.com>
2024-10-30 09:03:44 +0200

    Temp remove sitemap and tsconfigPaths

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), Sitemap({ hostname: 'https://studioz.co.il' })],
+  plugins: [react()],
   build: {
     outDir: 'dist'
   },
