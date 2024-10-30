0c1058618b31a960a9baa7e7ab94702721755a95
Darnell Green <123552242+G1320@users.noreply.github.com>
2024-10-30 09:08:43 +0200

    Add Sitemap plugin

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Sitemap({ hostname: 'https://studioz.co.il' })],
   build: {
     outDir: 'dist'
   },
