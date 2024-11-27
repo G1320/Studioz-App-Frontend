b707e563349da28d1dab2574fc3ce43b0fba03b1
Darnell Green <123552242+G1320@users.noreply.github.com>
2024-11-27 19:29:32 +0200

    Add GL analytics

--- src/components/layout/footer/MobileFooter.tsx
+++ src/components/layout/footer/MobileFooter.tsx
@@ -1,10 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import HomeIcon from '@mui/icons-material/Home';
 import GraphicEqIcon from '@mui/icons-material/GraphicEq';
 import SearchIcon from '@mui/icons-material/Search';
 import AddBusinessIcon from '@mui/icons-material/AddBusiness';
+import { useUserContext } from '@contexts/UserContext';
+import { toast } from 'sonner';
+import { useTranslation } from 'react-i18next';
 
 export const MobileFooter = () => {
+  const { user } = useUserContext();
+  const { t } = useTranslation('header');
+  const navigate = useNavigate();
+
+  const handleNavigate = (path: string) => {
+    if (!user?._id) {
+      toast.error(t('errors.login_required'));
+    } else {
+      navigate(path);
+    }
+  };
   return (
     <footer className="mobile-footer">
       <nav className="footer-grid">
@@ -18,7 +32,14 @@ export const MobileFooter = () => {
         <Link to="/search" className="footer-icon-link">
           <SearchIcon />
         </Link>
-        <Link to="/create-studio" className="footer-icon-link">
+        <Link
+          to="/create-studio"
+          onClick={(e) => {
+            e.preventDefault();
+            handleNavigate('/create-studio');
+          }}
+          className="footer-icon-link"
+        >
           <AddBusinessIcon />
         </Link>
         {/* Row 2: Text */}
@@ -31,7 +52,14 @@ export const MobileFooter = () => {
         <Link to="/search" className="footer-text-link">
           Search
         </Link>
-        <Link to="/create-studio" className="footer-text-link">
+        <Link
+          to="/create-studio"
+          onClick={(e) => {
+            e.preventDefault();
+            handleNavigate('/create-studio');
+          }}
+          className="footer-text-link"
+        >
           List Studio
         </Link>
       </nav>
