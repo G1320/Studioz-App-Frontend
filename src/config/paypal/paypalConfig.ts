24be5a534aa37c060bd66be835ab4500b03d2c4a
Darnell Green <123552242+G1320@users.noreply.github.com>
2024-11-28 18:25:12 +0200

    Update Paypal config

--- src/config/paypal/paypalConfig.ts
+++ src/config/paypal/paypalConfig.ts
@@ -1,9 +1,9 @@
 export const initialOptions = {
   clientId: import.meta.env.VITE_PAYPAL_CLIENT_ID,
-  'disable-funding': '',
-  'buyer-country': 'US',
+  // 'disable-funding': '',
+  // 'buyer-country': 'US',
   currency: 'USD',
-  'data-page-type': 'product-details',
-  components: 'buttons',
-  'data-sdk-integration-source': 'developer-studio'
+  // 'data-page-type': 'product-details',
+  components: 'buttons'
+  // 'data-sdk-integration-source': 'developer-studio'
 };
