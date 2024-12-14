6cf9db5ac653ced8934aca231fd9f2bec77bf87e
Darnell Green <123552242+G1320@users.noreply.github.com>
2024-12-14 08:16:51 +0200

    Fix FileUploader

--- src/services/file-upload-service.ts
+++ src/services/file-upload-service.ts
@@ -9,11 +9,9 @@ export const uploadFile = async (file: File): Promise<UploadFileResult> => {
     const formData = new FormData();
 
     formData.append('file', file);
-    formData.append('upload_preset', import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
     formData.append('cloud_name', import.meta.env.VITE_CLOUDINARY_CLOUD_NAME);
     formData.append('api_key', import.meta.env.VITE_CLOUDINARY_API_KEY);
-
-    formData.append('transformation', 'f_auto,q_auto,w_1000');
+    formData.append('upload_preset', import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
 
     const response = await fetch(cloudinaryEndpoint, {
       method: 'POST',
@@ -21,8 +19,9 @@ export const uploadFile = async (file: File): Promise<UploadFileResult> => {
     });
 
     if (!response.ok) {
-      console.log('response: ', response);
-      throw new Error('Failed to upload image');
+      const errorData = await response.json();
+      console.error('Cloudinary error:', errorData);
+      throw new Error(`Upload failed: ${errorData.error?.message || 'Unknown error'}`);
     }
 
     return await response.json();
