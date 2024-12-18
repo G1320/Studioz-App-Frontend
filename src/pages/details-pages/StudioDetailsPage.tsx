5aceaf1385fd1d11240fd163787ce4a3e158c9cd
Darnell Green <123552242+G1320@users.noreply.github.com>
2024-12-18 12:12:59 +0200

    Style ItemDetails

--- src/pages/details-pages/StudioDetailsPage.tsx
+++ src/pages/details-pages/StudioDetailsPage.tsx
@@ -50,12 +50,12 @@ const StudioDetailsPage: React.FC<StudioDetailsPageProps> = ({ items, cart }) =>
           </div>
         )}
       />
+      <ContinueToCheckoutButton cart={cart} />
       <Modal open={!!selectedItem} onClose={closeModal} className="item-modal">
         <div className="modal-content">
           {selectedItem && <ItemDetails cart={cart} studio={studioObj?.currStudio} item={selectedItem} />}
         </div>
       </Modal>
-      <ContinueToCheckoutButton cart={cart} />
     </section>
   );
 };
