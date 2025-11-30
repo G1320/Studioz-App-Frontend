ebaae16cb6840e098992d853613ded6037ecdcfb
Darnell Green <123552242+G1320@users.noreply.github.com>
2025-11-30 10:35:32 +0200

    Fix build errors

--- src/shared/hooks/utils/useLangNavigation.ts
+++ src/shared/hooks/utils/useLangNavigation.ts
@@ -1,12 +1,12 @@
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, NavigateOptions } from 'react-router-dom';
 
 export const useLanguageNavigate = () => {
   const { i18n } = useTranslation();
   const navigate = useNavigate();
 
-  return (path: string) => {
+  return (path: string, options?: NavigateOptions) => {
     const currentLang = i18n.language || 'en';
-    navigate(`/${currentLang}${path}`);
+    navigate(`/${currentLang}${path}`, options);
   };
 };
